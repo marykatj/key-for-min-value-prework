81b33e82aae72ea1e8d4867c533d96e85d497676
Mary-Kate James <marykatj@gmail.com>
2018-05-10 14:12:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,11 @@ def key_for_min_value(name_hash)
   if name_hash.length > 1
     min_key = ""
     name_hash.collect do |key, value|
-      key.sort
-      min_key = key.first
-      min_key
+        key.collect do |a, b|
+          a <=> b
+          min_key = a
+          min_key
+        end
     end
   else nil
   end
